1e059621fa92eff24ecb356b03db7c1a8c1a1128
Diego Andrés Toapanta <admaigualca@gmail.com>
2021-01-26 18:36:17 -0500

    service

--- service/src/main/java/com/espe/salud/service/paciente/PacienteServiceImpl.java
+++ service/src/main/java/com/espe/salud/service/paciente/PacienteServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Service("pacienteServiceImpl")
+@Service
 public class PacienteServiceImpl implements PacienteService {
 
     private final PacienteRepository pacienteRepository;
