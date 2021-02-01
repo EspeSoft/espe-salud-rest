1eb6604cd30cd4883d60670ffaaead622b8646cb
Mauricio Dávalos <madvalosj@espe.edu.ec>
2021-02-01 14:23:51 -0500

    CRUD Region Completado

--- app/src/main/java/com/espe/salud/app/common/Constants.java
+++ app/src/main/java/com/espe/salud/app/common/Constants.java
@@ -11,6 +11,7 @@ public class Constants {
     public static final String URI_API_V1_NOT_ENF = URI_API_V1 + "notas-enfermeria";
     public static final String URI_API_V1_EMP = URI_API_V1 + "empleados";
     public static final String URI_API_V1_EST = URI_API_V1 + "estudiantes";
+    public static final String URI_API_V1_REG = URI_API_V1 + "regiones";
 
 
 
---
+++ dto/src/main/java/com/espe/salud/dto/catalogo/RegionDTO.java
@@ -0,0 +1,18 @@
+package com.espe.salud.dto.catalogo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class RegionDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String nombre;
+
+
+}
+
