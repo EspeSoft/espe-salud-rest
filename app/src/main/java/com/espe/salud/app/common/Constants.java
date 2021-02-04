46753c39461544d2c0037e730d2f7239ac3ccd3c
Luis Pillaga <lapillaga@espe.edu.ec>
2021-02-04 01:29:25 -0500

    IMP: Códigos CIUO CRUD Terminado

--- app/src/main/java/com/espe/salud/app/common/Constants.java
+++ app/src/main/java/com/espe/salud/app/common/Constants.java
@@ -10,5 +10,6 @@ public class Constants {
     public static final String URI_API_V1_PAC = URI_API_V1 + "pacientes";
     public static final String URI_API_V1_NOT_ENF = URI_API_V1 + "notas-enfermeria";
     public static final String URI_API_V1_USU = URI_API_V1 + "usuarios";
+    public static final String URI_API_V1_COD_CIUO = URI_API_V1 + "codigos-ciuo";
 
 }
---
+++ persistence/src/main/java/com/espe/salud/persistence/catalogo/CodigoCIUORepository.java
@@ -0,0 +1,14 @@
+package com.espe.salud.persistence.catalogo;
+
+import com.espe.salud.domain.entities.catalogo.CodigoCIUO;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CodigoCIUORepository extends CrudRepository<CodigoCIUO, String> {
+    List<CodigoCIUO> findByCodigoStartingWithOrDescripcionStartingWith(String codigo, String descripcion);
+    Optional<CodigoCIUO> findByCodigo(String codigo);
+}
