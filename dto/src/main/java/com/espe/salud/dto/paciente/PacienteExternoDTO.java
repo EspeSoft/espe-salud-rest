30bd2e7bcb5c3f02023f646d59159b7e2f965038
Diego Andrés Toapanta <admaigualca@gmail.com>
2021-02-14 19:44:53 -0500

    Changes Mapper Paciente

--- dto/src/main/java/com/espe/salud/dto/paciente/PacienteExternoDTO.java
+++ dto/src/main/java/com/espe/salud/dto/paciente/PacienteExternoDTO.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import javax.validation.Valid;
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.List;
 
 @Getter
@@ -15,6 +16,31 @@ public class PacienteExternoDTO implements Serializable {
     private Boolean esEmpleado;
     private Boolean accesoBanner;
     private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String primerNombre;
+    private String segundoNombre;
+    private String cedula;
+    private LocalDate fechaNacimiento;
+    private String sexo;
+    private String estadoCivil;
+    private String religion;
+    private Boolean aceptaTransfucion;
+    private String grupoSanguineo;
+    private String lateralidad;
+    private String instruccion;
+    private String profesion;
+    private String vinculadoEspe;
+    private String pueblos;
+    private String grupoCultural;
+    private String seguroSalud;
+    private String asociacionAfiliada;
+    private String ciuo;
+    private Long idDispensario;
+    private String paisNacimiento;
+    private String provinciaNacimiento;
+    private String cantonNacimiento;
+    private String nacionalidad;
+    private String nacionalidad2;
 
     @Valid
     private ContactoDTO contacto;
