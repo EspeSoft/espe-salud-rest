cdce71945de4baee533de661ecde7c1cab02f48b
darexnigga <dgleon1@espe.edu.ec>
2021-02-17 00:59:40 -0500

    Task 34 - Finished (but with errors - have to fix)

--- domain/src/main/java/com/espe/salud/domain/entities/catalogo/TipoEstudioComplementario.java
+++ domain/src/main/java/com/espe/salud/domain/entities/catalogo/TipoEstudioComplementario.java
@@ -1,8 +1,10 @@
 package com.espe.salud.domain.entities.catalogo;
 
+import com.espe.salud.domain.entities.antecedente.EstudioComplementario;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Data
 @Entity
@@ -16,4 +18,7 @@ public class TipoEstudioComplementario {
 
     @Column(name = "MZSTCTIPESTCOM_NOMBRE")
     private String nombre;
+
+    @OneToMany(mappedBy = "nombreEstudio", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<EstudioComplementario> tipoEstudioComplementario;
 }
--- dto/src/main/java/com/espe/salud/dto/catalogo/TipoEstudioComplementarioDTO.java
+++ dto/src/main/java/com/espe/salud/dto/catalogo/TipoEstudioComplementarioDTO.java
@@ -8,21 +8,16 @@ import java.io.Serializable;
 
 @Getter
 @Setter
-<<<<<<< HEAD:dto/src/main/java/com/espe/salud/dto/odontologia/ExamenSistemaEstomatognaticoDTO.java
 public class ExamenSistemaEstomatognaticoDTO implements Serializable {
 
-=======
 public class TipoEstudioComplementarioDTO implements Serializable {
->>>>>>> origin/TASK-29:dto/src/main/java/com/espe/salud/dto/catalogo/TipoEstudioComplementarioDTO.java
     private static final long serialVersionUID = 1L;
 
     @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     private Long id;
-<<<<<<< HEAD:dto/src/main/java/com/espe/salud/dto/odontologia/ExamenSistemaEstomatognaticoDTO.java
     private String unidadEstomatognatica;
     private String codigoCIEAsociado;
     private Long historiaId;
-=======
     private String nombre;
->>>>>>> origin/TASK-29:dto/src/main/java/com/espe/salud/dto/catalogo/TipoEstudioComplementarioDTO.java
+
 }
